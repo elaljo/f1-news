d07e0895b7ebbdc2093c642bfc5767b3b49b14ce
Mohamed Elalj <elaljooo@gmail.com>
2026-03-10 12:32:08 +0000

    working with props

--- components/SelectedTeam.tsx
+++ components/SelectedTeam.tsx
@@ -1,18 +1,24 @@
 import styles from "./SelectedTeam.module.css";
-export default function SelectedTeam() {
+import type { Team } from "../types"
+
+type SelectedTeamProps = {
+    team: Team;
+}
+
+export default function SelectedTeam({team}: SelectedTeamProps) {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{backgroundImage: `url(${team.background})`}}>
         <div className={styles.leftPlayer}>
             <div className={styles.playerInfo}>
-                <span className={styles.playerNumber} style={{ backgroundImage: "url('/SelectedTeam/Ferrari/LeclercNumber.avif')" }}></span>
+                <span className={styles.playerNumber} style={{ backgroundImage: `url(${team.leftPlayer.number})` }}></span>
             </div>
-            <img className={styles.leftPlayerPhoto} src="/SelectedTeam/Ferrari/CharlesLeclerc.avif" alt="Charles Leclerc Photo" />
+            <img className={styles.leftPlayerPhoto} src={team.leftPlayer.image} alt={team.leftPlayer.name} />
         </div>
         <div className={styles.rightPlayer}>
             <div className={styles.playerInfo} style={{justifyContent: "end"}}>
-                <span className={styles.playerNumber} style={{ backgroundImage: "url('/SelectedTeam/Ferrari/HamiltonNumber.avif')"}}> </span>
+                <span className={styles.playerNumber} style={{ backgroundImage: `url(${team.rightPlayer.number})`}}> </span>
             </div>
-            <img className={styles.rightPlayerPhoto} src="/SelectedTeam/Ferrari/LewisHamilton.avif" alt="Lewis Hamilton Photo" />
+            <img className={styles.rightPlayerPhoto} src={team.rightPlayer.image} alt={team.rightPlayer.number} />
         </div>
     </div>
   );
