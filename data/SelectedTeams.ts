00310dbf22800e10a8da8926f5e1b71635b3e9f0
Mohamed Elalj <elaljooo@gmail.com>
2026-03-16 16:11:35 +0000

    set all f1 teams

--- data/SelectedTeams.ts
+++ data/SelectedTeams.ts
@@ -1,33 +1,158 @@
 import type { Team } from "../types"
 
 export const Teams: Team[] = [
-{
-    Team: "Ferrari",
-    background: "/SelectedTeam/Ferrari/Ferraribackground.png",
-    leftPlayer: {
-        name: "Charles Leclerc",
-        image: "/SelectedTeam/Ferrari/CharlesLeclerc.avif",
-        number: "/SelectedTeam/Ferrari/LeclercNumber.avif"
+    {
+        Team: "Mercedes",
+        background: "/SelectedTeam/Mercedes/MercedesBackground.png",
+        leftPlayer: {
+            name: "George Russell",
+            image: "/SelectedTeam/Mercedes/GeorgeRussell.avif",
+            number: "/SelectedTeam/Mercedes/RussellNumber.avif"
+        },
+        rightPlayer:{
+            name: "Kimi Antonelli",
+            image: "/SelectedTeam/Mercedes/KimiAntonelli.avif",
+            number: "/SelectedTeam/Mercedes/KimiNumber.avif"
+        }
     },
-    rightPlayer:{
-        name: "Lewis Hamilton",
-        image: "/SelectedTeam/Ferrari/LewisHamilton.avif",
-        number: "/SelectedTeam/Ferrari/HamiltonNumber.avif"
-    }
-},
-
-{
-    Team: "Red Bull Racing",
-    background: "/SelectedTeam/RedBull/RedBullBackground.png",
-    leftPlayer: {
-        name: "Isack Hadjar",
-        image: "/SelectedTeam/RedBull/IsackHadjar.avif",
-        number: "/SelectedTeam/RedBull/HadjarNumber.avif"
+    {
+        Team: "Ferrari",
+        background: "/SelectedTeam/Ferrari/Ferraribackground.png",
+        leftPlayer: {
+            name: "Charles Leclerc",
+            image: "/SelectedTeam/Ferrari/CharlesLeclerc.avif",
+            number: "/SelectedTeam/Ferrari/LeclercNumber.avif"
+        },
+        rightPlayer:{
+            name: "Lewis Hamilton",
+            image: "/SelectedTeam/Ferrari/LewisHamilton.avif",
+            number: "/SelectedTeam/Ferrari/HamiltonNumber.avif"
+        }
     },
-    rightPlayer:{
-        name: "Max Verstappen",
-        image: "/SelectedTeam/RedBull/MaxVerstappen.avif",
-        number: "/SelectedTeam/RedBull/MaxNumber.avif"
-    }
-}
+    {
+        Team: "McLaren",
+        background: "/SelectedTeam/McLaren/McLarenBackground.png",
+        leftPlayer: {
+            name: "Lando Norris",
+            image: "/SelectedTeam/McLaren/LandoNorris.avif",
+            number: "/SelectedTeam/McLaren/NorrisNumber.avif"
+        },
+        rightPlayer:{
+            name: "Oscar Piastri",
+            image: "/SelectedTeam/McLaren/OscarPiastri.avif",
+            number: "/SelectedTeam/McLaren/PiastriNumber.avif"
+        }
+    },
+    {
+        Team: "Haas",
+        background: "/SelectedTeam/Haas/HaasBackground.png",
+        leftPlayer: {
+            name: "esteban ocon",
+            image: "/SelectedTeam/Haas/EstebanOcon.avif",
+            number: "/SelectedTeam/Haas/OconNumber.avif"
+        },
+        rightPlayer:{
+            name: "oliver bearman",
+            image: "/SelectedTeam/Haas/OliverBearman.avif",
+            number: "/SelectedTeam/Haas/BearmanNumber.avif"
+        }
+    },
+    {
+        Team: "Red Bull Racing",
+        background: "/SelectedTeam/RedBull/RedBullBackground.png",
+        leftPlayer: {
+            name: "Isack Hadjar",
+            image: "/SelectedTeam/RedBull/IsackHadjar.avif",
+            number: "/SelectedTeam/RedBull/HadjarNumber.avif"
+        },
+        rightPlayer:{
+            name: "Max Verstappen",
+            image: "/SelectedTeam/RedBull/MaxVerstappen.avif",
+            number: "/SelectedTeam/RedBull/MaxNumber.avif"
+        }
+    },
+    {
+        Team: "Racing Bull",
+        background: "/SelectedTeam/Racing_Bull/Racing_BullBackground.png",
+        leftPlayer: {
+            name: "Liam Lawson",
+            image: "/SelectedTeam/Racing_Bull/LiamLawson.avif",
+            number: "/SelectedTeam/Racing_Bull/LawsonNumber.avif"
+        },
+        rightPlayer:{
+            name: "arvid lindblad",
+            image: "/SelectedTeam/Racing_Bull/ArvidLindblad.avif",
+            number: "/SelectedTeam/Racing_Bull/LindbladNumber.avif"
+        }
+    },
+    {
+        Team: "Alpine",
+        background: "/SelectedTeam/Alpine/AlpineBackground.png",
+        leftPlayer: {
+            name: "Pierre Gasly",
+            image: "/SelectedTeam/Alpine/PierreGasly.avif",
+            number: "/SelectedTeam/Alpine/GaslyNumber.avif"
+        },
+        rightPlayer:{
+            name: "Franco Colapinto",
+            image: "/SelectedTeam/Alpine/FrancoColapinto.avif",
+            number: "/SelectedTeam/Alpine/ColapintoNumber.avif"
+        }
+    },
+    {
+        Team: "Audi",
+        background: "/SelectedTeam/Audi/AudiBackground.png",
+        leftPlayer: {
+            name: "nico hulkenberg",
+            image: "/SelectedTeam/Audi/NicoHulkenberg.avif",
+            number: "/SelectedTeam/Audi/HulkenbergNumber.avif"
+        },
+        rightPlayer:{
+            name: "gabriel bortolotti",
+            image: "/SelectedTeam/Audi/GabrielBortolotti.avif",
+            number: "/SelectedTeam/Audi/BortolottiNumber.avif"
+        }
+    },
+    {
+        Team: "Williams",
+        background: "/SelectedTeam/Williams/WilliamsBackground.png",
+        leftPlayer: {
+            name: "carlos sainz",
+            image: "/SelectedTeam/Williams/CarlosSainz.avif",
+            number: "/SelectedTeam/Williams/SainzNumber.avif"
+        },
+        rightPlayer:{
+            name: "alexander albon",
+            image: "/SelectedTeam/Williams/AlexanderAlbon.avif",
+            number: "/SelectedTeam/Williams/AlbonNumber.avif"
+        }
+    },
+    {
+        Team: "cadillac",
+        background: "/SelectedTeam/Cadillac/CadillacBackground.png",
+        leftPlayer: {
+            name: "sergio perez",
+            image: "/SelectedTeam/Cadillac/SergioPerez.avif",
+            number: "/SelectedTeam/Cadillac/PerezNumber.avif"
+        },
+        rightPlayer:{
+            name: "valterri bottas",
+            image: "/SelectedTeam/Cadillac/ValterriBottas.avif",
+            number: "/SelectedTeam/Cadillac/BottasNumber.avif"
+        }
+    },
+    {
+        Team: "Aston Martin",
+        background: "/SelectedTeam/Aston_Martin/AstonMartinBackground.png",
+        leftPlayer: {
+            name: "Fernando Alonso",
+            image: "/SelectedTeam/Aston_Martin/FernandoAlonso.avif",
+            number: "/SelectedTeam/Aston_Martin/AlonsoNumber.avif"
+        },
+        rightPlayer:{
+            name: "Lance Stroll",
+            image: "/SelectedTeam/Aston_Martin/LanceStroll.avif",
+            number: "/SelectedTeam/Aston_Martin/StrollNumber.avif"
+        }
+    },
 ]
