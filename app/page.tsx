fed8765e86b661a8aeb0bb5f04e6a909b07e1359
Mohamed Elalj <elaljooo@gmail.com>
2026-04-07 15:58:09 +0100

    removed drivers from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import SelectedTeam from "@/components/SelectedTeam";
-import {Teams} from "../data/SelectedTeams"
-
 export default function Home() {
   return (
-    <div style={{display: "flex", flexWrap: "wrap", gap: "30px", padding: "50px", justifyContent: "center"}}>
-    {Teams.map((team) => (
-      <SelectedTeam key={team.Team} team={team} />
-    ))}
-  </div>
+    <>
+      <h1>Home</h1>
+    </>
   );
 }
