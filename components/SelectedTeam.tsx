a70f7f03532f79d9cabb538bd8d28ef781cc6621
Mohamed Elalj <elaljooo@gmail.com>
2026-04-07 15:56:08 +0100

    modified on the selectedTeam cards..

--- components/SelectedTeam.tsx
+++ components/SelectedTeam.tsx
@@ -1,25 +1,60 @@
+"use client";
+
 import styles from "./SelectedTeam.module.css";
-import type { Team } from "../types"
+import type { Team } from "../types";
+import { motion } from "framer-motion";
 
 type SelectedTeamProps = {
-    team: Team;
-}
+  team: Team;
+  itemIndex?: number;
+  delayStep?: number;
+  manualDelay?: number;
+};
 
-export default function SelectedTeam({team}: SelectedTeamProps) {
+export default function SelectedTeam({
+  team,
+  itemIndex = 0,
+  delayStep = 0.2,
+  manualDelay,
+}: SelectedTeamProps) {
+  const delay = manualDelay ?? itemIndex * delayStep;
+
   return (
-    <div className={styles.container} style={{backgroundImage: `url(${team.background})`}}>
-        <div className={styles.leftPlayer}>
-            <div className={styles.playerInfo}>
-                <span className={styles.playerNumber} style={{ backgroundImage: `url(${team.leftPlayer.number})` }}></span>
-            </div>
-            <img className={styles.leftPlayerPhoto} src={team.leftPlayer.image} alt={team.leftPlayer.name} />
+    <motion.div
+      className={styles.container}
+      style={{ backgroundImage: `url(${team.background})` }}
+      initial={{ opacity: 0, x: -150 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.3, ease: "easeOut", delay }}
+    >
+      <div className={styles.leftPlayer}>
+        <div className={styles.playerInfo}>
+          <span
+            className={styles.playerNumber}
+            style={{ backgroundImage: `url(${team.leftPlayer.number})` }}
+          ></span>
         </div>
-        <div className={styles.rightPlayer}>
-            <div className={styles.playerInfo} style={{justifyContent: "end"}}>
-                <span className={styles.playerNumber} style={{ backgroundImage: `url(${team.rightPlayer.number})`}}> </span>
-            </div>
-            <img className={styles.rightPlayerPhoto} src={team.rightPlayer.image} alt={team.rightPlayer.number} />
+        <img
+          className={styles.leftPlayerPhoto}
+          src={team.leftPlayer.image}
+          alt={team.leftPlayer.name}
+        />
+      </div>
+      <div className={styles.rightPlayer}>
+        <div className={styles.playerInfo} style={{ justifyContent: "end" }}>
+          <span
+            className={styles.playerNumber}
+            style={{ backgroundImage: `url(${team.rightPlayer.number})` }}
+          >
+            {" "}
+          </span>
         </div>
-    </div>
+        <img
+          className={styles.rightPlayerPhoto}
+          src={team.rightPlayer.image}
+          alt={team.rightPlayer.number}
+        />
+      </div>
+    </motion.div>
   );
 }
--- data/SelectedTeams.ts
+++ data/SelectedTeams.ts
@@ -2,20 +2,6 @@ import type { Team } from "../types"
 
 export const Teams: Team[] = [
     {
-        Team: "Mercedes",
-        background: "/SelectedTeam/Mercedes/MercedesBackground.png",
-        leftPlayer: {
-            name: "George Russell",
-            image: "/SelectedTeam/Mercedes/GeorgeRussell.avif",
-            number: "/SelectedTeam/Mercedes/RussellNumber.avif"
-        },
-        rightPlayer:{
-            name: "Kimi Antonelli",
-            image: "/SelectedTeam/Mercedes/KimiAntonelli.avif",
-            number: "/SelectedTeam/Mercedes/KimiNumber.avif"
-        }
-    },
-    {
         Team: "Ferrari",
         background: "/SelectedTeam/Ferrari/Ferraribackground.png",
         leftPlayer: {
@@ -30,19 +16,20 @@ export const Teams: Team[] = [
         }
     },
     {
-        Team: "McLaren",
-        background: "/SelectedTeam/McLaren/McLarenBackground.png",
+        Team: "Red Bull Racing",
+        background: "/SelectedTeam/RedBull/RedBullBackground.png",
         leftPlayer: {
-            name: "Lando Norris",
-            image: "/SelectedTeam/McLaren/LandoNorris.avif",
-            number: "/SelectedTeam/McLaren/NorrisNumber.avif"
+            name: "Isack Hadjar",
+            image: "/SelectedTeam/RedBull/IsackHadjar.avif",
+            number: "/SelectedTeam/RedBull/HadjarNumber.avif"
         },
         rightPlayer:{
-            name: "Oscar Piastri",
-            image: "/SelectedTeam/McLaren/OscarPiastri.avif",
-            number: "/SelectedTeam/McLaren/PiastriNumber.avif"
+            name: "Max Verstappen",
+            image: "/SelectedTeam/RedBull/MaxVerstappen.avif",
+            number: "/SelectedTeam/RedBull/MaxNumber.avif"
         }
     },
+   
     {
         Team: "Haas",
         background: "/SelectedTeam/Haas/HaasBackground.png",
@@ -55,20 +42,6 @@ export const Teams: Team[] = [
             name: "oliver bearman",
             image: "/SelectedTeam/Haas/OliverBearman.avif",
             number: "/SelectedTeam/Haas/BearmanNumber.avif"
-        }
-    },
-    {
-        Team: "Red Bull Racing",
-        background: "/SelectedTeam/RedBull/RedBullBackground.png",
-        leftPlayer: {
-            name: "Isack Hadjar",
-            image: "/SelectedTeam/RedBull/IsackHadjar.avif",
-            number: "/SelectedTeam/RedBull/HadjarNumber.avif"
-        },
-        rightPlayer:{
-            name: "Max Verstappen",
-            image: "/SelectedTeam/RedBull/MaxVerstappen.avif",
-            number: "/SelectedTeam/RedBull/MaxNumber.avif"
         }
     },
     {
@@ -86,73 +59,101 @@ export const Teams: Team[] = [
         }
     },
     {
-        Team: "Alpine",
-        background: "/SelectedTeam/Alpine/AlpineBackground.png",
+        Team: "McLaren",
+        background: "/SelectedTeam/McLaren/McLarenBackground.png",
         leftPlayer: {
-            name: "Pierre Gasly",
-            image: "/SelectedTeam/Alpine/PierreGasly.avif",
-            number: "/SelectedTeam/Alpine/GaslyNumber.avif"
+            name: "Lando Norris",
+            image: "/SelectedTeam/McLaren/LandoNorris.avif",
+            number: "/SelectedTeam/McLaren/NorrisNumber.avif"
         },
         rightPlayer:{
-            name: "Franco Colapinto",
-            image: "/SelectedTeam/Alpine/FrancoColapinto.avif",
-            number: "/SelectedTeam/Alpine/ColapintoNumber.avif"
+            name: "Oscar Piastri",
+            image: "/SelectedTeam/McLaren/OscarPiastri.avif",
+            number: "/SelectedTeam/McLaren/PiastriNumber.avif"
         }
     },
-    {
-        Team: "Audi",
-        background: "/SelectedTeam/Audi/AudiBackground.png",
+     {
+        Team: "Mercedes",
+        background: "/SelectedTeam/Mercedes/MercedesBackground.png",
         leftPlayer: {
-            name: "nico hulkenberg",
-            image: "/SelectedTeam/Audi/NicoHulkenberg.avif",
-            number: "/SelectedTeam/Audi/HulkenbergNumber.avif"
+            name: "George Russell",
+            image: "/SelectedTeam/Mercedes/GeorgeRussell.avif",
+            number: "/SelectedTeam/Mercedes/RussellNumber.avif"
         },
         rightPlayer:{
-            name: "gabriel bortolotti",
-            image: "/SelectedTeam/Audi/GabrielBortolotti.avif",
-            number: "/SelectedTeam/Audi/BortolottiNumber.avif"
+            name: "Kimi Antonelli",
+            image: "/SelectedTeam/Mercedes/KimiAntonelli.avif",
+            number: "/SelectedTeam/Mercedes/KimiNumber.avif"
         }
     },
-    {
-        Team: "Williams",
-        background: "/SelectedTeam/Williams/WilliamsBackground.png",
-        leftPlayer: {
-            name: "carlos sainz",
-            image: "/SelectedTeam/Williams/CarlosSainz.avif",
-            number: "/SelectedTeam/Williams/SainzNumber.avif"
-        },
-        rightPlayer:{
-            name: "alexander albon",
-            image: "/SelectedTeam/Williams/AlexanderAlbon.avif",
-            number: "/SelectedTeam/Williams/AlbonNumber.avif"
-        }
-    },
-    {
-        Team: "cadillac",
-        background: "/SelectedTeam/Cadillac/CadillacBackground.png",
-        leftPlayer: {
-            name: "sergio perez",
-            image: "/SelectedTeam/Cadillac/SergioPerez.avif",
-            number: "/SelectedTeam/Cadillac/PerezNumber.avif"
-        },
-        rightPlayer:{
-            name: "valterri bottas",
-            image: "/SelectedTeam/Cadillac/ValterriBottas.avif",
-            number: "/SelectedTeam/Cadillac/BottasNumber.avif"
-        }
-    },
-    {
-        Team: "Aston Martin",
-        background: "/SelectedTeam/Aston_Martin/AstonMartinBackground.png",
-        leftPlayer: {
-            name: "Fernando Alonso",
-            image: "/SelectedTeam/Aston_Martin/FernandoAlonso.avif",
-            number: "/SelectedTeam/Aston_Martin/AlonsoNumber.avif"
-        },
-        rightPlayer:{
-            name: "Lance Stroll",
-            image: "/SelectedTeam/Aston_Martin/LanceStroll.avif",
-            number: "/SelectedTeam/Aston_Martin/StrollNumber.avif"
-        }
-    },
+    // {
+        //     Team: "Alpine",
+        //     background: "/SelectedTeam/Alpine/AlpineBackground.png",
+        //     leftPlayer: {
+            //         name: "Pierre Gasly",
+            //         image: "/SelectedTeam/Alpine/PierreGasly.avif",
+            //         number: "/SelectedTeam/Alpine/GaslyNumber.avif"
+    //     },
+    //     rightPlayer:{
+    //         name: "Franco Colapinto",
+    //         image: "/SelectedTeam/Alpine/FrancoColapinto.avif",
+    //         number: "/SelectedTeam/Alpine/ColapintoNumber.avif"
+    //     }
+    // },
+    // {
+    //     Team: "Audi",
+    //     background: "/SelectedTeam/Audi/AudiBackground.png",
+    //     leftPlayer: {
+    //         name: "nico hulkenberg",
+    //         image: "/SelectedTeam/Audi/NicoHulkenberg.avif",
+    //         number: "/SelectedTeam/Audi/HulkenbergNumber.avif"
+    //     },
+    //     rightPlayer:{
+    //         name: "gabriel bortolotti",
+    //         image: "/SelectedTeam/Audi/GabrielBortolotti.avif",
+    //         number: "/SelectedTeam/Audi/BortolottiNumber.avif"
+    //     }
+    // },
+    // {
+    //     Team: "Williams",
+    //     background: "/SelectedTeam/Williams/WilliamsBackground.png",
+    //     leftPlayer: {
+    //         name: "carlos sainz",
+    //         image: "/SelectedTeam/Williams/CarlosSainz.avif",
+    //         number: "/SelectedTeam/Williams/SainzNumber.avif"
+    //     },
+    //     rightPlayer:{
+    //         name: "alexander albon",
+    //         image: "/SelectedTeam/Williams/AlexanderAlbon.avif",
+    //         number: "/SelectedTeam/Williams/AlbonNumber.avif"
+    //     }
+    // },
+    // {
+    //     Team: "cadillac",
+    //     background: "/SelectedTeam/Cadillac/CadillacBackground.png",
+    //     leftPlayer: {
+    //         name: "sergio perez",
+    //         image: "/SelectedTeam/Cadillac/SergioPerez.avif",
+    //         number: "/SelectedTeam/Cadillac/PerezNumber.avif"
+    //     },
+    //     rightPlayer:{
+    //         name: "valterri bottas",
+    //         image: "/SelectedTeam/Cadillac/ValterriBottas.avif",
+    //         number: "/SelectedTeam/Cadillac/BottasNumber.avif"
+    //     }
+    // },
+    // {
+    //     Team: "Aston Martin",
+    //     background: "/SelectedTeam/Aston_Martin/AstonMartinBackground.png",
+    //     leftPlayer: {
+    //         name: "Fernando Alonso",
+    //         image: "/SelectedTeam/Aston_Martin/FernandoAlonso.avif",
+    //         number: "/SelectedTeam/Aston_Martin/AlonsoNumber.avif"
+    //     },
+    //     rightPlayer:{
+    //         name: "Lance Stroll",
+    //         image: "/SelectedTeam/Aston_Martin/LanceStroll.avif",
+    //         number: "/SelectedTeam/Aston_Martin/StrollNumber.avif"
+    //     }
+    // },
 ]
