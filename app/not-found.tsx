67d8c418e68720d9130b7d43617a2628bd683fef
Mohamed Elalj <elaljooo@gmail.com>
2026-04-16 15:57:29 +0100

    create the page of not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="relative min-h-screen w-full overflow-hidden text-white p-20">
+        <img src="/PageNotFound/Hamilton.png"/>
+    </main>
+  );
+}
